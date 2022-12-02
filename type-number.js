c79e334bb963b5d96328d4cfaf2e8740d69532c4
Gian <gianhabeck@gmail.com>
2022-12-02 17:32:19 -0300

    primeiro commit

---
+++ type-number.js
@@ -0,0 +1,19 @@
+// Tipo Number
+const meuNumero = 3;
+const primeiroNumero = 1;
+const segundoNumero = 2;
+
+const operacaoMatematica = primeiroNumero * segundoNumero;
+console.log(operacaoMatematica);
+
+// ponto flutuante
+
+const numeroPontoFlutuante = 3.3;
+const segundoNumeroFlutuante = .5;
+const operacaoFlutuante = numeroPontoFlutuante / segundoNumeroFlutuante;
+console.log(operacaoFlutuante);
+
+// Nan -> Not A Number (Nao e um numero)
+
+const texto = "Alura";
+console.log(texto * primeiroNumero)
---
+++ type-string.js
@@ -0,0 +1,16 @@
+const texto1 = "Ola, mundo!";
+const texto2 = 'Ola, mundo!';
+const senha = "senhasupersegura456!";
+const strngDeNumeros = "345667";
+
+const citacao = 'Meu nome e ';
+const nome = "Gian";
+console.log(citacao);
+
+// template string OU template literal
+
+const testeString = `template`;
+console.log(testeString);
+
+// concatenacao (+)
+console.log(citacao + nome);
