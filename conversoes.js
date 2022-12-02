c7ee5fdeb71ab1ca59d1ba92d88749f4e260c482
Gian <gianhabeck@gmail.com>
2022-12-02 18:46:07 -0300

    Novas aulas realizadas

---
+++ conversoes.js
@@ -0,0 +1,13 @@
+// tipo de dado
+// boolean
+
+// conversao implicita
+const numero = 456;
+const numeroString = "456";
+console.log(numero + numeroString)
+
+
+// conversao explicita
+//Number()
+//String()
+console.log(numero + Number(numeroString))
